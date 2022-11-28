f39a710529c15b7101a8c466263d68f3e3f6f70e
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-11-28 21:56:16 +0300

    Добавьте файлы проекта.

---
+++ WebLessons15ASP1/MeetingSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebLessons15ASP1
+{
+    public class MeetingSettings
+    {
+
+            public DateTime DateMeeting { get; set; }
+            public int MaxPeople { get; set; }
+            public int TimeMeetingMin { get; set; }
+        
+    }
+}
---
+++ WebLessons15ASP1/ServiceMeeting.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace WebLessons15ASP1
+{
+    public class ServiceMeeting
+    {
+        private readonly MeetingSettings _settings;
+        public ServiceMeeting(IOptions<MeetingSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public string GetSettings()
+        {
+            return $"MaxPeople:{_settings.MaxPeople} , TimeMeetingMin {_settings.TimeMeetingMin}";
+        }
+    }
+}
